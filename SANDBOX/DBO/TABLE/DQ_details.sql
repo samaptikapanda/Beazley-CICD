52d897127f1b808fabe1d35151b560a11127d958
Mohan <mohan.sahoo@performalytic.com>
2024-05-11 18:33:09 +0530

    DQ_details Drop Column

--- SANDBOX/DBO/TABLE/DQ_details.sql
+++ SANDBOX/DBO/TABLE/DQ_details.sql
@@ -2,6 +2,5 @@ create or replace TABLE "DBO"."DQ_details" (
 	DETAIL_ID NUMBER(38,0) autoincrement start 1 increment 1 noorder,
 	ORDER_ID NUMBER(38,0),
 	PRODUCT_ID NUMBER(38,0),
-	QUANTITY NUMBER(38,0),
-	PRICE NUMBER(10,2)
+	QUANTITY NUMBER(38,0)
 );
