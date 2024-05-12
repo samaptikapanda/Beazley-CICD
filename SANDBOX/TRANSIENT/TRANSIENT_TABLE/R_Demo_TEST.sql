c57811fe91f2c2110647e9bcc7fc3687fe758f28
Mohan <mohan.sahoo@performalytic.com>
2024-05-12 15:50:50 +0530

    New or drop column

--- SANDBOX/TRANSIENT/TRANSIENT_TABLE/R_Demo_TEST.sql
+++ SANDBOX/TRANSIENT/TRANSIENT_TABLE/R_Demo_TEST.sql
@@ -2,7 +2,6 @@ create or replace TRANSIENT TABLE "TRANSIENT"."R_Demo_TEST" (
 	"Name" VARCHAR(50) NOT NULL,
 	"Emp_ID" NUMBER(20,0) NOT NULL autoincrement start 2 increment 2 noorder,
 	SALARY NUMBER(18,0),
-	DOB DATE NOT NULL,
 	"Col2" NUMBER(38,0) NOT NULL autoincrement start 1 increment 1 noorder,
 	"Col3" DATE NOT NULL
 );
