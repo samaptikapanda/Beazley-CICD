4ec388029909f28cc206b4d3a86761beeeda69a4
samaptikapanda <samaptikapanda>
2024-05-12 16:10:28 +0530

    Deployment Script changes by FDALERT user

--- Migration/script.sql
+++ Migration/script.sql
@@ -1,16 +1,9 @@
-ALTER TABLE "SANDBOX1"."DBO".CUSTOMERSV1 ALTER "NAME" SET DATA TYPE VARCHAR(400);
+ALTER TABLE "SANDBOX1"."DBO".CUSTOMERSV1 ALTER "NAME" SET DATA TYPE VARCHAR(500);
 -----------------------------------------------------------------------------
 
 -----------------------------------------------------------------------------
-create or replace TRANSIENT TABLE "TRANSIENT"."Customer_Test" (
-	EMP_ID NUMBER(38,0),
-	EMP_NAME VARCHAR(50),
-	AGE NUMBER(38,0),
-	CURRENT_ADDRESS VARCHAR(100),
-	EMAIL VARCHAR(100),
-	BUSINESS VARCHAR(100),
-	PHONE VARCHAR(20),
-	JOINING_DATE DATE,
-	SALARY FLOAT
+create or replace TABLE "DBO".CUSTOMERSV2 (
+	ID NUMBER(38,0) NOT NULL autoincrement start 1 increment 1 noorder,
+	NAME VARCHAR(100) NOT NULL
 );
 -----------------------------------------------------------------------------
