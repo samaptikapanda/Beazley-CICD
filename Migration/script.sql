afddf09467715e635961ac6066d7aa41910e4d03
samaptikapanda <samaptikapanda>
2024-05-12 16:40:52 +0530

    Deployment Script changes by FDALERT user

--- Migration/script.sql
+++ Migration/script.sql
@@ -1,15 +1,9 @@
-create or replace TRANSIENT TABLE "TRANSIENT"."Test_Table" (
-	EMP_ID NUMBER(38,0) NOT NULL,
-	ID NUMBER(38,0),
-	EMP_NAME VARCHAR(50),
-	AGE NUMBER(38,0),
-	CURRENT_ADDRESS VARCHAR(100),
-	EMAIL VARCHAR(100),
-	BUSINESS VARCHAR(100),
-	PHONE VARCHAR(20),
-	JOINING_DATE DATE,
-	SALARY FLOAT,
-	primary key (EMP_ID),
-	foreign key (ID) references "TRANSIENT"."Test_Table"("EMP_ID")
+create or replace TABLE "DBO".CUSTOMERSV3 (
+	ID NUMBER(38,0) NOT NULL autoincrement start 1 increment 1 noorder,
+	NAME VARCHAR(100) NOT NULL,
+	ADDR1 VARCHAR(20),
+	ADDR2 NUMBER(38,0),
+	ADDR3 DATE,
+	ADDR4 VARCHAR(16777216)
 );
 -----------------------------------------------------------------------------
