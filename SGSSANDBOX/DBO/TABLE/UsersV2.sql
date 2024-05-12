2dd7a60633739d8717d335a6777f9313e2c8fedf
Mohan <mohan.sahoo@performalytic.com>
2024-05-12 10:40:17 +0530

    uv2

--- SGSSANDBOX/DBO/TABLE/UsersV2.sql
+++ SGSSANDBOX/DBO/TABLE/UsersV2.sql
@@ -1,4 +1,5 @@
 create or replace TABLE "DBO"."UsersV2" (
 	ID NUMBER(38,0) autoincrement start 1 increment 1 noorder,
-	NAME VARCHAR(100) NOT NULL
+	NAME VARCHAR(100) NOT NULL,
+	"Age" NUMBER(38,0)
 );
