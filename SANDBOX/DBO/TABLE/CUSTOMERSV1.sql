5026b7ec19ee6b717ca7c583023d076bbc282e14
Mohan <mohan.sahoo@performalytic.com>
2024-05-12 11:58:20 +0530

    CUSTOMERV1 WITH ONE NEW COLUMN

--- SANDBOX/DBO/TABLE/CUSTOMERSV1.sql
+++ SANDBOX/DBO/TABLE/CUSTOMERSV1.sql
@@ -1,4 +1,5 @@
 create or replace TABLE "DBO".CUSTOMERSV1 (
 	ID NUMBER(38,0) NOT NULL autoincrement start 1 increment 1 noorder,
-	NAME VARCHAR(100) NOT NULL
+	NAME VARCHAR(100) NOT NULL,
+	ADDR VARCHAR(20)
 );
