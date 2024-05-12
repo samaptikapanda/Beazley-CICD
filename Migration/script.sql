00899f77824a17eaf188047f320bc457f101be92
samaptikapanda <samaptikapanda>
2024-05-12 16:32:21 +0530

    Deployment Script changes by FDALERT user

--- Migration/script.sql
+++ Migration/script.sql
@@ -1,7 +1,15 @@
-ALTER TABLE "SANDBOX1"."DBO".CUSTOMERSV1 ADD constraint PK_CUSTOMERS primary key (ID)
-;
------------------------------------------------------------------------------
-
------------------------------------------------------------------------------
-ALTER TABLE "SANDBOX1"."DBO"."Users" ALTER "Age" SET DATA TYPE NUMBER(10,0);
+create or replace TRANSIENT TABLE "TRANSIENT"."Test_Table" (
+	EMP_ID NUMBER(38,0) NOT NULL,
+	ID NUMBER(38,0),
+	EMP_NAME VARCHAR(50),
+	AGE NUMBER(38,0),
+	CURRENT_ADDRESS VARCHAR(100),
+	EMAIL VARCHAR(100),
+	BUSINESS VARCHAR(100),
+	PHONE VARCHAR(20),
+	JOINING_DATE DATE,
+	SALARY FLOAT,
+	primary key (EMP_ID),
+	foreign key (ID) references "TRANSIENT"."Test_Table"("EMP_ID")
+);
 -----------------------------------------------------------------------------
