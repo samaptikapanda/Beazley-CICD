6a7159fbd057a5b9e49c59c4ded0858bf425c612
Mohan <mohan.sahoo@performalytic.com>
2024-05-12 13:10:51 +0530

    ADD COLUMN

--- Migration/script.sql
+++ Migration/script.sql
@@ -1,25 +1,7 @@
-ALTER TABLE "SANDBOX1"."DBO".CUSTOMERSV1 ADD ADDR2 VARCHAR(20)
+ALTER TABLE ""."DBO".DEPLOYMENT_TEST ADD NEW_COLUMN VARCHAR(16777216);
+-----------------------------------------------------------------------------
+ALTER TABLE ""."DBO".DEPLOYMENT_TEST ADD DATE_DELETED VARCHAR(16777216)
 ;
 -----------------------------------------------------------------------------
 
------------------------------------------------------------------------------
-create or replace TABLE "DBO".DEPLOYMENT_TEST (
-	ID NUMBER(38,0),
-	NAME VARCHAR(16777216),
-	DATE_CREATED TIMESTAMP_NTZ(9)
-);
------------------------------------------------------------------------------
-
------------------------------------------------------------------------------
-create or replace TRANSIENT TABLE "TRANSIENT"."Employee_Summary" (
-	EMP_ID NUMBER(38,0),
-	EMP_NAME VARCHAR(50),
-	AGE NUMBER(38,0),
-	CURRENT_ADDRESS VARCHAR(100),
-	EMAIL VARCHAR(100),
-	BUSINESS VARCHAR(100),
-	PHONE VARCHAR(20),
-	JOINING_DATE DATE,
-	SALARY FLOAT
-);
 -----------------------------------------------------------------------------
