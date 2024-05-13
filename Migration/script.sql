1a577b103f6a3d9ff7d166da85ed4f249dcadab7
samaptikapanda <samaptikapanda>
2024-05-13 11:50:11 +0530

    Deployment Script changes by FDALERT user

--- Migration/script.sql
+++ Migration/script.sql
@@ -1,3 +1,13 @@
+create or replace TABLE "DBO".TEST_123 (
+	AA1 VARCHAR(30) NOT NULL DEFAULT '10avf',
+	BB1 BINARY(10),
+	CC22 NUMBER(38,0) NOT NULL autoincrement start 1 increment 1 order,
+	DD1 DATE,
+	primary key (AA1, CC22)
+);
+-----------------------------------------------------------------------------
+
+-----------------------------------------------------------------------------
 ALTER TABLE "SANDBOX1"."DBO"."Users" DROP COLUMN ID;
 -----------------------------------------------------------------------------
 
@@ -7,9 +17,13 @@ ALTER TABLE "DBO"."UsersV3" ADD ADDR8 VARCHAR(20)
 -----------------------------------------------------------------------------
 ALTER TABLE "SANDBOX1"."DBO"."UsersV3" DROP COLUMN ADDR5;
 -----------------------------------------------------------------------------
+ALTER TABLE "SANDBOX1"."DBO"."UsersV3" DROP COLUMN ADDR6;
+-----------------------------------------------------------------------------
 
 -----------------------------------------------------------------------------
-ALTER TABLE "DBO".YES ADD ADDR17 VARCHAR(20)
+ALTER TABLE "DBO".YES ADD ADDR17 VARCHAR(20);
+-----------------------------------------------------------------------------
+ALTER TABLE "DBO".YES ADD ADDR18 VARCHAR(20)
 ;
 -----------------------------------------------------------------------------
 
