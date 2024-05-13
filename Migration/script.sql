ff2da19b4ff598ae70d2a96f08c078645e4f6d9a
samaptikapanda <samaptikapanda>
2024-05-13 12:26:19 +0530

    Deployment Script changes by FDALERT user

--- Migration/script.sql
+++ Migration/script.sql
@@ -1,3 +1,5 @@
-ALTER TABLE "DBO".YES ADD ADDR21 VARCHAR(20)
+ALTER TABLE "DBO".YES ADD ADDR21 VARCHAR(20);
+-----------------------------------------------------------------------------
+ALTER TABLE "DBO".YES ADD ADDR22 VARCHAR(20)
 ;
 -----------------------------------------------------------------------------
