da2914e863bd5d2239b6a72a19b5e11f79a232ca
Mohan <mohan.sahoo@performalytic.com>
2024-05-15 18:03:18 +0530

    test

---
+++ SANDBOX/DBO/TABLE/CUSTOMERSV1.sql
@@ -0,0 +1,8 @@
+create or replace TABLE "DBO".CUSTOMERSV1 (
+	ID NUMBER(38,0) NOT NULL autoincrement start 1 increment 1 noorder,
+	NAME VARCHAR(500) NOT NULL,
+	ADDR VARCHAR(20),
+	ADDR2 VARCHAR(20),
+	ADDR1 VARCHAR(500),
+	constraint PK_CUSTOMERS primary key (ID)
+);
