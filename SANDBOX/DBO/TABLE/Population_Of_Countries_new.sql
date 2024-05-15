cb6625fe90bfff3c0ef6e99059b78faec4512413
Mohan <mohan.sahoo@performalytic.com>
2024-05-15 18:03:46 +0530

    test

---
+++ SANDBOX/DBO/TABLE/Population_Of_Countries_new.sql
@@ -0,0 +1,13 @@
+create or replace TABLE "DBO"."Population_Of_Countries_new" (
+	"country_id" NUMBER(38,0),
+	"country_name" VARCHAR(255),
+	"state_name" VARCHAR(255),
+	"district_name" VARCHAR(255),
+	"country_population_in_millions" NUMBER(38,0),
+	"state_population_in_millions" NUMBER(38,0),
+	"district_population_in_millions" NUMBER(38,0),
+	"population_year" DATE,
+	"literacy_rate" NUMBER(5,2),
+	"country_currency" VARCHAR(50),
+	"uniquenumber" VARCHAR(255)
+);
