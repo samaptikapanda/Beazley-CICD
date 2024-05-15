65ae8de41cc2adce2ec387f353b14b4917ad9767
Mohan <mohan.sahoo@performalytic.com>
2024-05-15 18:03:26 +0530

    test

---
+++ SANDBOX/DBO/TABLE/DimJobTask_Details.sql
@@ -0,0 +1,21 @@
+create or replace TABLE "DBO"."DimJobTask_Details" (
+	"JobItemId" NUMBER(38,0),
+	"JobTaskId" NUMBER(38,0),
+	"ProductionTaskId" NUMBER(38,0),
+	"JobTaskDescription" VARCHAR(100),
+	"JobTaskStatus" VARCHAR(15),
+	"JobTaskStatusDateTime" DATE,
+	"IsJobTaskNeedingApproval" BOOLEAN,
+	"JobTaskCompletedDateTime" DATE,
+	"JobTaskOperatorLoginId" NUMBER(38,0),
+	"EstimatedDuration" NUMBER(19,0),
+	"IsJobTaskQC" BOOLEAN,
+	"JobTaskQCStatusFromTaskHistory" VARCHAR(50),
+	"JobTaskQCOperatorLoginIdFromTaskHistory" NUMBER(38,0),
+	"TaskCategoryForPlanning" VARCHAR(100),
+	"JobTaskStatusFromTaskHistory" VARCHAR(50),
+	"JobTaskStatusDateTimeFromTaskHistory" DATE,
+	"JobTaskOperatorLoginIdFromTaskHistory" NUMBER(38,0),
+	"ProductionTaskGroup" VARCHAR(3),
+	"LastUpdatedDate" DATE
+);
