492d39ffd00a0d601428413b3b83094787ca733e
Mohan <mohan.sahoo@performalytic.com>
2024-05-15 18:03:24 +0530

    test

---
+++ SANDBOX/DBO/TABLE/DQ_details.sql
@@ -0,0 +1,6 @@
+create or replace TABLE "DBO"."DQ_details" (
+	DETAIL_ID NUMBER(38,0) autoincrement start 1 increment 1 noorder,
+	ORDER_ID NUMBER(38,0),
+	PRODUCT_ID NUMBER(38,0),
+	QUANTITY NUMBER(38,0)
+);
