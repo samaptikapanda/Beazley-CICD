9a7242dba526c3f498b0bed578e484b14c7c78a7
Mohan <mohan.sahoo@performalytic.com>
2024-05-15 18:03:16 +0530

    test

---
+++ SANDBOX/DBO/TABLE/AB_Demo_PRIMARY_FOREIGN.sql
@@ -0,0 +1,8 @@
+create or replace TABLE "DBO"."AB_Demo_PRIMARY_FOREIGN" (
+	"order_id" NUMBER(38,0) NOT NULL,
+	"order_date" DATE NOT NULL,
+	"total_amount" NUMBER(10,2) NOT NULL,
+	"customer_id" NUMBER(38,0),
+	primary key ("order_id"),
+	foreign key ("customer_id") references DBO."AB_Demo_PRIMARY"(Emp_ID)
+);
