e376a842c5f48bb191a8d05ed2e4c5b0ab789e3f
niharrout <niharrout>
2024-05-20 22:46:59 +0530

    Deployment Script changes by FDALERT user

--- Migration/script.sql
+++ Migration/script.sql
@@ -1,14 +1,35 @@
-create or replace materialized view "TRANSIENT"."mv_product_sales"(
-	"product_id",
-	TOTAL_SALES,
-	NUMBER_OF_SALES
-) as
-SELECT
-    "product_id",
-    SUM(amount) AS total_sales,
-    COUNT(*) AS number_of_sales
-FROM
-    "sales"
-GROUP BY
-    "product_id";
+CREATE OR REPLACE PROCEDURE "TRANSIENT"."CURSOR_Demo"("EMPNAME" VARCHAR(50))
+RETURNS VARCHAR(16777216)
+LANGUAGE SQL
+EXECUTE AS OWNER
+AS '
+begin
+ LET res RESULTSET := (select EMPNAME from "SNOWFLAKE_TEST"."DEV"."STGTEST"  where EXPERIANCE >5);
+ LET item_cursor CURSOR for res;
+ LET all_people VARCHAR := '''';
+ For v_item_cursor in item_cursor do
+ all_people :=v_item_cursor.EMPNAME;
+ INSERT INTO  "SNOWFLAKE_TEST"."DEV"."TEMPR"  (EMPNAME) values(:all_people);
+
+ END FOR;
+  INSERT INTO  "SNOWFLAKE_TEST"."DEV"."TEMPR02" 
+   (EMPID,
+   EMPNAME,
+   EMPADDRESS,
+   EXPERIANCE,
+   SALRY,
+   BLOODGROUP
+   )
+   SELECT 
+   a.EMPID,
+   a.EMPNAME,
+   a.EMPADDRESS,
+   a.EXPERIANCE,
+   a.SALRY,
+   a.BLOODGROUP
+   FROM "SNOWFLAKE_TEST"."DEV"."STGTEST" as a,
+   "SNOWFLAKE_TEST"."DEV"."TEMPR" as b
+   where a.EMPNAME = b.EMPNAME;
+end
+';
 -----------------------------------------------------------------------------
