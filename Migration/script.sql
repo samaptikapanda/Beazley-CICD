8fdd2a5f9232e0fbd49d16efae658d08d16bdc67
niharrout <niharrout>
2024-05-21 23:27:57 +0530

    Deployment Script changes by FDALERT user

---
+++ Migration/script.sql
@@ -0,0 +1,20 @@
+create or replace TABLE "DBO".ABCD (
+	EMPLOYEE_ID NUMBER(38,0),
+	FIRST_NAME VARCHAR(50),
+	LAST_NAME VARCHAR(50),
+	DEPARTMENT VARCHAR(100),
+	STATE VARCHAR(200),
+	"Age2" VARCHAR(200),
+	"Age5" VARCHAR(200),
+	"Age25" VARCHAR(200)
+);
+-----------------------------------------------------------------------------
+
+-----------------------------------------------------------------------------
+create or replace view "DBO".VS(
+	ID,
+	NAME
+) as
+SELECT ID,NAME
+FROM "DBO"."PERF";
+-----------------------------------------------------------------------------
