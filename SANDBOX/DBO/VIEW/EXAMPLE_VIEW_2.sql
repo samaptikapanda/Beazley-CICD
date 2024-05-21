7ce9b36bb59e8652a84aca3f6e92e02a0a6dc187
samaptika <samaptika.panda@performalytic.com>
2024-05-21 23:36:47 +0530

    test

---
+++ SANDBOX/DBO/VIEW/EXAMPLE_VIEW_2.sql
@@ -0,0 +1,14 @@
+create or replace view "DBO".EXAMPLE_VIEW_2(
+	ID,
+	UNIQUE_COLUMN,
+	OTHER_COLUMN,
+	NULL_COLUMN
+) as
+SELECT
+    ID,
+    UNIQUE_COLUMN,
+    OTHER_COLUMN,
+    "Test_Column" AS NULL_COLUMN
+FROM
+     "DBO".EXAMPLE_TABLE
+    where ID > 2 ;
