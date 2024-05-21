0fe3997dfc11203ffd724c0fba4751dde027c821
Sam <79141193+samaptikapanda@users.noreply.github.com>
2024-05-22 00:44:01 +0530

    Delete SANDBOX/DBO directory

--- SANDBOX/DBO/FUNCTION/EVEN_OR_ODD.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE FUNCTION "DBO"."EVEN_OR_ODD"("INPUT_NUMBER" NUMBER(38,0))
-RETURNS VARCHAR(16777216)
-LANGUAGE SQL
-AS '
-    -- Check if the input number is even or odd
-    CASE WHEN MOD(input_number,10) <> 0 THEN ''Odd'' ELSE ''Even'' END
-  ';
