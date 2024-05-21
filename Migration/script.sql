1b1dec217858f692a09eb2d7ba3e08af6f826dca
niharrout <niharrout>
2024-05-21 14:48:27 +0530

    Deployment Script changes by FDALERT user

--- Migration/script.sql
+++ Migration/script.sql
@@ -1,3 +1,30 @@
+CREATE OR REPLACE FUNCTION "FDA"."FETCH_TABLE_DATA_DEV"()
+RETURNS VARIANT
+LANGUAGE JAVASCRIPT
+AS '
+    var schemaName = 
+    var tableName = "SP_EMPDETAILS";
+    
+    var sqlQuery = `SELECT * FROM "DBO"."SP_EMPDETAILS"`;
+    
+    var stmt = snowflake.createStatement({sqlText: sqlQuery});
+    var resultSet = stmt.execute();
+    
+    var result = [];
+    while (resultSet.next()) {
+      var row = {};
+      for (var i = 1; i <= resultSet.getColumnCount(); i++) {
+        var columnName = resultSet.getColumnName(i);
+        row[columnName] = resultSet.getColumnValue(i);
+      }
+      result.push(row);
+    }
+    
+    return result;
+  ';
+-----------------------------------------------------------------------------
+
+-----------------------------------------------------------------------------
 CREATE OR REPLACE PROCEDURE "FDA"."GET_ORDER_DETAILS_DEV"()
 RETURNS TABLE ("ORDER_ID" NUMBER(38,0), "ORDER_DATE" DATE, "CUSTOMER_ID" NUMBER(38,0), "ORDER_AMOUNT" NUMBER(38,0), "TAX_RATE" NUMBER(38,0), "TOTAL_PRICE_WITH_TAX" NUMBER(38,0))
 LANGUAGE SQL
