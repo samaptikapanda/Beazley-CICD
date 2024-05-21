9b7e92660da3e4bce6a2e8850dfd7b60c303f8bb
niharrout <niharrout>
2024-05-21 14:33:28 +0530

    Deployment Script changes by FDALERT user

--- Migration/script.sql
+++ Migration/script.sql
@@ -1,9 +1,22 @@
-create or replace TRANSIENT TABLE "FDA"."Demo" (
-	"Name" VARCHAR(50) NOT NULL,
-	"Emp_ID" NUMBER(20,0) NOT NULL autoincrement start 2 increment 2 noorder,
-	"Gender" VARCHAR(50),
-	SALARY NUMBER(18,0),
-	DOB DATE NOT NULL,
-	ADDRESS VARCHAR(50)
-);
+CREATE OR REPLACE PROCEDURE "FDA"."GET_ORDER_DETAILS_DEV"()
+RETURNS TABLE ("ORDER_ID" NUMBER(38,0), "ORDER_DATE" DATE, "CUSTOMER_ID" NUMBER(38,0), "ORDER_AMOUNT" NUMBER(38,0), "TAX_RATE" NUMBER(38,0), "TOTAL_PRICE_WITH_TAX" NUMBER(38,0))
+LANGUAGE SQL
+EXECUTE AS OWNER
+AS '
+DECLARE
+    cur_result CURSOR FOR
+        SELECT 
+            order_id,
+            order_date,
+            customer_id,
+            order_amount,
+            tax_rate,
+            total_price_with_tax
+        FROM 
+            order_details_with_total_price;
+BEGIN
+    OPEN cur_result;
+    RETURN TABLE(cur_result);
+END;
+';
 -----------------------------------------------------------------------------
