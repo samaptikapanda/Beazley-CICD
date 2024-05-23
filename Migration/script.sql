fceb6f32747ddb530dcd12d6b3ce303f8b5e992d
niharrout <niharrout>
2024-05-23 10:30:38 +0530

    Deployment Script changes by FDALERT user

---
+++ Migration/script.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE PROCEDURE "DBO"."INSERTEMPLOYEE_RANJAN_7"("P_FIRSTNAME" VARCHAR(50), "P_LASTNAME" VARCHAR(50), "P_BIRTHDATE" DATE, "P_HIREDATE" DATE, "P_POSITION" VARCHAR(100), "P_SALARY" NUMBER(10,2))
+RETURNS VARCHAR(16777216)
+LANGUAGE SQL
+EXECUTE AS OWNER
+AS '
+DECLARE
+    v_EmployeeID INT;
+BEGIN
+    -- Get the next value from the sequence
+    v_EmployeeID := NEXTVAL(''EmployeeIDSeq'');
+    
+    -- Insert the new employee
+    INSERT INTO Employees (EmployeeID, FirstName, LastName, BirthDate, HireDate)
+    VALUES (v_EmployeeID, p_FirstName, p_LastName, p_BirthDate, p_HireDate);
+    
+    RETURN ''Employee inserted successfully with EmployeeID: '' || v_EmployeeID;
+END;
+';
+-----------------------------------------------------------------------------
