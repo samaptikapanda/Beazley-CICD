093fc18f919c0f43b0ffbf694e402e4ce38ed1b5
samaptikapanda <samaptikapanda>
2024-05-24 14:08:48 +0530

    Deployment Script changes by FDALERT user

--- Migration/script.sql
+++ Migration/script.sql
@@ -3,3 +3,10 @@ FROM @"Stage_RUNU"
 FILE_FORMAT = (FORMAT_NAME = "my_csv_format")
 ON_ERROR = 'CONTINUE';
 -----------------------------------------------------------------------------
+
+-----------------------------------------------------------------------------
+create or replace pipe "FDA"."PIPE_Test2" auto_ingest=false as COPY INTO "FDA"."RUNU_DimEmployee"
+FROM @"FDA"."Stage_RUNU"
+FILE_FORMAT = (FORMAT_NAME = "FDA"."my_csv_format")
+ON_ERROR = 'CONTINUE';
+-----------------------------------------------------------------------------
