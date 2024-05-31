5d94d2d42ce26e49db56046c55a1873ae8308967
Sam <79141193+samaptikapanda@users.noreply.github.com>
2024-05-31 12:33:01 +0530

    Delete Migration directory

--- Migration/script.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE FDA.EXAMPLE_TABLE ADD primary key (ID);
------------------------------------------------------------------------------
-ALTER TABLE FDA.EXAMPLE_TABLE ADD foreign key (MANAGER_ID) references SGSSANDBOX.FDA.EXAMPLE_TABLE("ID");
