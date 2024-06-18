18a4d9f0b108b699c1885ee01767e1b74f246071
Sam <79141193+samaptikapanda@users.noreply.github.com>
2024-06-18 17:18:48 +0530

    Delete Migration directory

--- Migration/script.sql
+++
@@ -1,3 +0,0 @@
-create or replace sequence DBO.EMPLOYEEIDSEQ_MOHAN start with 1 increment by 1 noorder;
------------------------------------------------------------------------------
-create or replace sequence FDA.DEV_TEST start with 2 increment by 2 noorder;
